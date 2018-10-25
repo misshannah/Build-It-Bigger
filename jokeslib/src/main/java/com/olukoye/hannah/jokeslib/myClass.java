1ae493ce58ea45ec8ef81b294b0f9b8d2dfcbcd9
hannaholukoye <hannaholukoye@gmail.com>
2018-10-25 23:16:49 +0300

    Updates: Edits on readme file and initial commit of the source code for review.

---
+++ jokeslib/src/main/java/com/olukoye/hannah/jokeslib/myClass.java
@@ -0,0 +1,8 @@
+package com.olukoye.hannah.jokeslib;
+
+public class myClass {
+    public static String getJoke() {
+        return "Where do polar bears keep their money? In a snow bank!";
+    }
+
+}
